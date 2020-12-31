fa5081425cc4a56a47832071d7785646ef212d7f
Thomas <tpnats@gmail.com>
2020-12-31 14:26:56 -0500

    7th commit,fix data files, created bar chart on html

--- HTML/Happiness.js
+++ HTML/Happiness.js
@@ -1,7 +1,36 @@
-//Assign variable to data
-var happy2018 = data2018;
-var happy2019 = data2019;
-//console.log(happy2018[0])
-//console.log(happy2019[0])
 
+function filterHappyCountries(country) {
+    return country.Overall_rank < 16;
+}
 
+var filteredCountries = data2018.filter(filterHappyCountries);
+
+//console.log(filteredCountries);
+
+var countries = filteredCountries.map(countries => countries.Country_or_region);
+
+//console.log(countries);
+
+var happiness = filteredCountries.map(countries => countries.Happiness_Score);
+
+console.log(happiness);
+
+// 5. Create your trace.
+var trace = {
+  x: countries,
+  y: happiness,
+  type: "bar"
+};
+
+// 6. Create the data array for our plot
+var data = [trace];
+
+// 7. Define our plot layout
+var layout = {
+  title: "Top 10 Happiest Countries in 2019",
+  xaxis: { title: "Country" },
+  yaxis: { title: "Happiness Score"}
+};
+
+// 8. Plot the chart to a div tag with id "bar-plot"
+Plotly.newPlot("bar-plot", data, layout);
